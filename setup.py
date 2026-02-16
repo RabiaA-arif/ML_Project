bb268ac1ecf59b80b4ef150ca4fbdabed70ad560
RabiaA-arif <rabiaarif694@gmail.com>
2026-02-16 18:58:34 +0500

    setting

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages ,setup
+
+setup(
+    name='mlproject',
+    versions='0.0.1',
+    author='Rabia',
+    author_email='',
+    packages=find_packages(),
+    install_requires=['pandas','numpy','seaborn']
+)
